d58f894c472792304b11943ac737f2b0cf203c99
Terry Tan <fuling19.me@gmail.com>
2024-01-21 16:59:29 +0800

    添加配置类

--- fims/src/main/java/pers/terry/fims/FimsApplication.java
+++ fims/src/main/java/pers/terry/fims/FimsApplication.java
@@ -3,10 +3,10 @@ package pers.terry.fims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
-@EnableJpaAuditing
-@EnableOpenApi
+@EnableJpaRepositories(basePackages = "pers.terry.fims.repository")
 @SpringBootApplication
 public class FimsApplication {
 
---
+++ fims/src/main/java/pers/terry/fims/config/SpringDataJpaConfig.java
@@ -0,0 +1,66 @@
+package pers.terry.fims.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@Getter
+@Setter
+@PropertySource(value = "classpath:application.yaml")
+@ConfigurationProperties(prefix = "spring.datasource")
+public class SpringDataJpaConfig {
+    private String username;
+    private String password;
+    private String driverClassName;
+    private String url;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setUsername(this.username);
+        dataSource.setPassword(this.password);
+        dataSource.setDriverClassName(this.driverClassName);
+        dataSource.setJdbcUrl(this.url);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("pers.terry.fims.entity");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
